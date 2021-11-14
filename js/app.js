d22f071b8872d393015aacd837460a94158fcc43
Sourav <srvz39@gmail.com>
2021-11-15 00:40:15 +0530

    local commit

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var dropdownElementList = [].slice.call(document.querySelectorAll('.dropdown-toggle'))
+/* var dropdownElementList = [].slice.call(document.querySelectorAll('.dropdown-toggle'))
 var dropdownList = dropdownElementList.map(function (dropdownToggleEl) {
   return new bootstrap.Dropdown(dropdownToggleEl)
 })
@@ -11,3 +11,4 @@ function add_new_task(ele){
   $(task_row).append("<div class='col-12'></div>")
   $(task_row).find('.col-12').load('/card_template.html')
 }
+ */
